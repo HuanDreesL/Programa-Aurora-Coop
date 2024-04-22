193f28b98cf278be39500eeb6abbed1db936ce71
HuanDreesL <huandres-schmidt@auroracoop.com.br>
2024-04-22 13:46:10 -0300

    Programa V1.1

--- aurora_coop.dart
+++ aurora_coop.dart
@@ -2,8 +2,17 @@ void main(){
 
 Produto produto1 = Produto('Linguiça Pernil', 450, 5.0, 18,89, 3);
 Produto produto2 = Produto('Empanado', 550, 1.0, 10,99, 3);
+produto1.adicionaEstoque(3);
+produto2.adicionaEstoque(3);
+produto1.descontaEstoque(2);
+produto2.descontaEstoque(2);
+produto1.calculaPrecoTotal(3);
+produto2.calculaPrecoTotal(3);
+
+
 Cliente cliente1 = Cliente('Brasão', 12345678, 5000);
 Cliente cliente2 = Cliente('Alfa', 87654321, 5000);
+
 Venda venda1 = Venda(cliente1, [produto1], 283.35);
 Venda venda2 = Venda(cliente2, [produto2], 32.97);
 venda1.venda(12345678, [450]);
